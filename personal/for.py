6e5c0534782eef63e4032a711bb6f967780442eb
caomaoxia <123456>
2021-01-25 21:48:04 +0800

    ..

---
+++ personal/for.py
@@ -0,0 +1,16 @@
+family={
+    'father':'caoshujun',
+    'mother':'lishiqing',
+    'daughter':"caomaoxia",
+    "boyfriend":'daiyifan'}
+print(family.keys())
+print(family.items())
+for member in family.keys():
+     if member=='daughter':
+         print("I'm "+family[member]+'.')
+     else:
+         if member=='mother':
+             print('This is my ' + member + ', her name is '+family[member])
+         else:
+             print( 'This is my ' + member + ', his name is '+family[member])
+
---
+++ xianxian/spam.py
@@ -0,0 +1,8 @@
+#将列表信息返回为字符串
+family_members=['caoshujun','lishiqing','caomaoxia','daiyifan','tuanzi']
+print(','.join(family_members[:-1])+' and '+family_members[-1])
+
+
+
+
+
